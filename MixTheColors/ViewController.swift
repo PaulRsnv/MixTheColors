f0e2125a85acb5ab7602ee497cbfb5320248efaa
Oxyenniu <oxyenniu@gmail.com>
2023-09-13 05:19:35 +0300

    work with segmentControl

--- MixTheColors/ViewController.swift
+++ MixTheColors/ViewController.swift
@@ -11,37 +11,62 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     
-    @IBOutlet weak var redColorValue: UILabel!
-    @IBOutlet weak var greenColorValue: UILabel!
-    @IBOutlet weak var blueColorValue: UILabel!
+    @IBOutlet weak var redTextLabel: UILabel!
+    @IBOutlet weak var greenTextLabel: UILabel!
+    @IBOutlet weak var blueTextLabel: UILabel!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    
+    @IBOutlet weak var choosingSegmentControl: UISegmentedControl!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.layer.cornerRadius = 20
+        colorView.layer.cornerRadius = 15
         setColor()
-        setValue(for: redColorValue, greenColorValue, blueColorValue)
+        setValue(from: redValueLabel, greenValueLabel, blueValueLabel)
         
     }
+    
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
+        let hideRedString = (sender.selectedSegmentIndex == 1)
+        let hideGreenString = (sender.selectedSegmentIndex == 2)
+        let hideBlueString = (sender.selectedSegmentIndex == 3)
+        
+        redTextLabel.isHidden = hideRedString
+        redValueLabel.isHidden = hideRedString
+        redSlider.isHidden = hideRedString
+
+        greenTextLabel.isHidden = hideGreenString
+        greenValueLabel.isHidden = hideGreenString
+        greenSlider.isHidden = hideGreenString
+
+        blueTextLabel.isHidden = hideBlueString
+        blueValueLabel.isHidden = hideBlueString
+        blueSlider.isHidden = hideBlueString
+        }
+    
+
+    
     
     @IBAction func rgbSliderAction(_ sender: UISlider) {
         setColor()
         switch sender {
         case redSlider:
-            redColorValue.text = string(from: redSlider)
+            redValueLabel.text = string(from: redSlider)
         case greenSlider:
-            greenColorValue.text = string(from: greenSlider)
+            greenValueLabel.text = string(from: greenSlider)
         default:
-            blueColorValue.text = string(from: blueSlider)
+            blueValueLabel.text = string(from: blueSlider)
         }
-        
     }
     
     private func setColor() {
@@ -49,27 +74,26 @@ class ViewController: UIViewController {
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
-            alpha: 1.0
-        )
+            alpha: 1.0)
     }
     
-    private func setValue(for labels: UILabel...) {
+    private func setValue(from labels: UILabel...) {
         labels.forEach { label in
             switch label {
-            case redColorValue:
-                redColorValue.text = string(from: redSlider)
-            case greenColorValue:
-                greenColorValue.text = string(from: greenSlider)
+            case redValueLabel:
+                redValueLabel.text = string(from: redSlider)
+            case greenValueLabel:
+                greenValueLabel.text = string(from: greenSlider)
             default:
-                blueColorValue.text = string(from: blueSlider)
+                blueValueLabel.text = string(from: blueSlider)
             }
         }
     }
     
-    private func string(from slider: UISlider) -> String{
-        String(format: "%.2f", slider.value)
-    }
     
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.0f", slider.value)
+    }
 
 }
 
